462206ac5832da4c489486b6ffab6b1f685986c5
alextanhongpin <alextan220990@gmail.com>
2020-01-02 00:22:09 +0800

    chore: initial commit

---
+++ src/Application.kt
@@ -0,0 +1,111 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import io.ktor.locations.*
+import org.slf4j.event.*
+import io.ktor.http.content.*
+import io.ktor.util.date.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    install(Locations) {
+    }
+
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    install(CachingHeaders) {
+        options { outgoingContent ->
+            when (outgoingContent.contentType?.withoutParameters()) {
+                ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60), expires = null as? GMTDate?)
+                else -> null
+            }
+        }
+    }
+
+    install(DataConversion)
+
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+
+    install(ForwardedHeaderSupport) // WARNING: for security, do not include this if not behind a reverse proxy
+    install(XForwardedHeaderSupport) // WARNING: for security, do not include this if not behind a reverse proxy
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+
+        get<MyLocation> {
+            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
+        }
+        // Register nested routes
+        get<Type.Edit> {
+            call.respondText("Inside $it")
+        }
+        get<Type.List> {
+            call.respondText("Inside $it")
+        }
+
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+    }
+}
+
+@Location("/location/{name}")
+class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+
+@Location("/type/{name}") data class Type(val name: String) {
+    @Location("/edit")
+    data class Edit(val type: Type)
+
+    @Location("/list/{page}")
+    data class List(val type: Type, val page: Int)
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
+
